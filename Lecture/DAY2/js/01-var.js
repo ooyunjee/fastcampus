de130c6f567c0e3d46b3e506983c94796aacfb6d
heoyunjee <heoyunjee@gmail.com>
2016-09-27 15:09:19 +0900

    Add Lecture-DAY2

---
+++ Lecture/DAY2/js/01-var.js
@@ -0,0 +1,27 @@
+// TypeScript(JavaScript)
+// Jade(HTML), Sass(css)
+
+// 동작 구현(Action, Behavior)
+// 프론트엔드 웹 환경에서 HTML과 함께
+// 백엔드 환경에서는 혼자 동작 가능
+
+// 행동1. 변수(Variable) 정의
+var x = 10;
+var y = x + 9;
+var language;
+
+// 웹 브라우저 세상에는 콘솔이라고 불리는 객체(사물, Object)가 있다.
+// 콘솔(console)에게 이 데이터를 참조한 변수를 화면에 기록(log)하라고 시킴.
+console.log('x: ', x);
+console.log('x: ', y);
+
+// 선언(정의)된 변수 x에 값(데이터)을 할당
+// x = 10;
+
+// 사칙연산
+// x = x + 3; // 6
+// y = x + 9; // x값이 10이라면? y값은? 19
+
+// 문자 접합(용접)
+language = 'This is ' + 'JavaScript.'; // this is JavaScript
+console.log('language: ' + language);
