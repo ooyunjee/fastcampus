04c86f986fd8375d0d78af8c67fe5129ff4bc70e
heoyunjee <heoyunjee@gmail.com>
2016-10-11 11:14:58 +0900

    Delete team activity in DAY8 folder

--- Lecture/DAY8/js/team.js
+++
@@ -1,8 +0,0 @@
-// function showGuide () {
-//   var body = document.body;
-//   var currentClass = body.getAttribute('class');
-//
-//   body.setAttribute('class', currentClass + ' guide');
-// }
-//
-// document.onclick = showGuide;
