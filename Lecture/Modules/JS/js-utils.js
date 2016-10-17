90d932e0c161f0d2faa6a9e409766a291d8e68b6
heoyunjee <heoyunjee@gmail.com>
2016-10-18 02:08:42 +0900

    Refactor js utils Module

--- Lecture/Modules/JS/js-utils.js
+++ Lecture/Modules/JS/js-utils.js
@@ -1,4 +1,18 @@
-// isType()
+/**
+ * @function isType 데이터 유형을 정확하게 감지하는 함수
+ * @param  {everything}  data
+ * @return {string}      데이터 유형을 문자열로 반환
+ */
+
 function isType(data) {
   return Object.prototype.toString.call(data).toLowerCase().slice(8, -1);
+}
+
+/**
+ * [isExist description]
+ * @param  {[type]}  node [description]
+ * @return {Boolean}      [description]
+ */
+function isExist(node) {
+  return isType(node) !== 'null';
 }
